5e42f5082516712e75cb846c248d6b52f0725912
Vladimir Rudnev <vladimir.rudnev@jetbrains.com>
2021-12-01 11:26:24 +0500

    day 01

--- tasks/src/day01/day01.kt
+++ tasks/src/day01/day01.kt
@@ -1,9 +1,28 @@
 package day01
 
-fun solve() {
+import common.Common
 
+fun solve(): Int {
+    val numbers = Common.getNumbers("tasks/src/day01/in.txt")
+    var ans = 0
+    for (i in 1 until numbers.size) {
+        if (numbers[i] > numbers[i - 1]) ans++
+    }
+    return ans
+}
+
+fun solve2(): Int {
+    val numbers = Common.getNumbers("tasks/src/day01/in.txt")
+    var ans = 0
+    for (i in 1 until numbers.size - 2) {
+        val sum1 = numbers[i - 1] + numbers[i] + numbers[i + 1]
+        val sum2 = sum1 - numbers[i - 1] + numbers[i + 2]
+        if (sum1 < sum2) ans++
+    }
+    return ans
 }
 
 fun main() {
-    solve()
+    println(solve())
+    println(solve2())
 }
